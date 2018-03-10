721dd2191a09e3960d4717040734d8ac3b7ad06b
tsktsktsky <2654886+tsktsktsky@users.noreply.github.com>
2018-03-10 17:01:26 +0800

    Live logger update

--- livelog.sh
+++ livelog.sh
@@ -1,3 +1,8 @@
-#!/bin/bash    
+#!/bin/bash
 
-watch --color "git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --all -20"
+while :
+do
+	    clear
+	        git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --all -20
+		    sleep 1
+	    done
