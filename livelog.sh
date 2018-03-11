72110363a895952bedfde4a078002e136a9b354b
tsktsktsky <2654886+tsktsktsky@users.noreply.github.com>
2018-03-11 17:32:26 +0800

    Livelog to show only the SHA and the one line commit message

--- livelog.sh
+++ livelog.sh
@@ -3,6 +3,6 @@
 while :
 do
 	    clear
-	        git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative --all -20
+	        git --no-pager log --graph --oneline --decorate --abbrev-commit --date=relative --all -20
 		    sleep 1
 	    done
